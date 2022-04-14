058f99eff0109deae2bf35d5232412d820996635
shab <shabanali@MacBook-Pro.local>
2022-04-14 11:41:42 +0100

    changes to node_modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from "./components/header";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      That is brilliant
     </div>
   );
 }
