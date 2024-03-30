5c1c922aa8445a910c85ff2497a69d02d731b2f2
shab <shabanali@MacBook-Pro.local>
2024-03-30 11:23:23 +0000

    counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from "./components/header";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <Header />
       That is brilliant
+      <div>
+        <button onClick={() => setCount(count + 1)}>click me</button>
+      </div>
+      <div>Current count: {count}</div>
     </div>
   );
 }
--- src/components/__test__/header.test.js
+++ src/components/__test__/header.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Header from "../header";
 
 test("Header component renders header title", () => {
@@ -9,7 +10,7 @@ test("Header component renders header title", () => {
   let text = screen.getByText(/This is the header mate/i);
 
   // assert that this element is in the dom or component
-  //expect(text).toBeInTheDocument();
+  expect(text).toBeInTheDocument();
   expect(text.textContent).toBe("This is the header mate");
   // get the element to test the textcontent
 });
